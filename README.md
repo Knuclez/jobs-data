ef6e36b64431073a1aa9462a49749b8da6411743
mdiazmoraiz <mdiazmoraiz@dc.uba.ar>
2022-06-10 20:34:45 -0300

    actualizo readme y arranco el objetivo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jobs-data
 Manejo basico de pandas y datos
+
+<h4>Objetivo del momento:</h4>
+La columna de sueldos que tiene min - max, partirla en dos columnas diferentes, despues pasarlas a enteros ambas para poder graficar.
--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import matplotlib
 from pyparsing import Regex
 
 df = pd.read_csv("./Naukri Jobs Data.csv")
-
 #print(df.head)
 #print(df.dtypes), queria saber con que trabajaba, imprimir head no me deja ver todas las columnas
 #print(df.salary_offered)
 
-df2 = df.salary_offered.str.replace('PA.','', regex = False)
-print(df2.value_counts())
+serie_salary = df.salary_offered.str.replace('PA.','', regex = False)
+#logre modificar un dato string interno. Sueldo tiene rango (min - max), podre partirlo en dos columnas? 
+
